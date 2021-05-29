d6d03bb17c79745e7b7847cd8c6f30f2f83bbdd9
Erselan Khan <karsalan@foree.co>
2021-05-29 16:43:42 +0500

    enable development for autoreload

--- src/Application.kt
+++ src/Application.kt
@@ -2,13 +2,10 @@ package com.erselan
 
 import io.ktor.application.*
 import io.ktor.response.*
-import io.ktor.request.*
 import io.ktor.routing.*
 import io.ktor.http.*
 import io.ktor.gson.*
 import io.ktor.features.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -18,22 +15,14 @@ fun Application.module(testing: Boolean = false) {
     install(ContentNegotiation) {
         gson {
         }
-
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
     }
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            call.respondText("HELLO!", contentType = ContentType.Text.Plain)
         }
 
         get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
-        }
-
-        get("/json/jackson") {
             call.respond(mapOf("hello" to "world"))
         }
     }
