794fe27097abb8b43181ce241c5e6307ddd2796f
Erselan Khan <karsalan@foree.co>
2021-05-30 19:58:29 +0500

    create UserEntity data class and add plugin for UserEntity

---
+++ src/entity/UserEntity.kt
@@ -0,0 +1,14 @@
+package com.erselan.entity
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserEntity(
+    private val id: Int,
+    private val firstName: String,
+    private val lastName: String,
+    private val phoneNumber: String,
+    private val emailAddress: String,
+    private val password: String,
+    private val confirmPassword: String
+)
--- src/route/UserRoutes.kt
+++ src/route/UserRoutes.kt
@@ -1,10 +1,11 @@
 package com.erselan.route
 
 import com.erselan.constant.RoutesConstant
+import com.erselan.entity.UserEntity
 import io.ktor.application.*
-import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import kotlin.random.Random
 
 /*
 Define Appilication extension function for User Routes
@@ -16,13 +17,20 @@ fun Application.userRoutes() {
 }
 
 private fun Route.userRoutes() {
+    val usersList = mutableListOf<UserEntity>()
+    for (i in 1..5) {
+        usersList.add(UserEntity(id = i, firstName = "User $i", lastName = "Khan",
+            phoneNumber = Random.nextInt(1, 100).toString(), emailAddress = "email $i",
+            password = "12345", confirmPassword = "12345"))
+    }
+
     /*
     Define route string as /user
     so all calls related to user are handled by this route function
     */
     route(RoutesConstant.USER_ROUTES) {
         get {
-            call.respondText(text = "Arsalan Khan", status = HttpStatusCode.OK)
+            call.respond(usersList)
         }
     }
 }
