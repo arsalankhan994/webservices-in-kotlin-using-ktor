99593e46f1c93c77de531e0fb549d1ab17f9886f
Erselan Khan <karsalan@foree.co>
2021-05-30 18:31:37 +0500

    basic initial example

--- src/Application.kt
+++ src/Application.kt
@@ -1,40 +1,26 @@
 package com.erselan
 
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.gson.*
 import io.ktor.features.*
-import com.fasterxml.jackson.databind.*
-import io.ktor.jackson.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.serialization.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(ContentNegotiation) {
-        gson {
-        }
-
-        jackson {
-            enable(SerializationFeature.INDENT_OUTPUT)
-        }
+        json()
     }
 
     routing {
-        get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
-
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
-        }
-
-        get("/json/jackson") {
-            call.respond(mapOf("hello" to "world"))
+        get {
+            call.respondText(
+                "Hello World",
+                status = HttpStatusCode.OK
+            )
         }
     }
 }
