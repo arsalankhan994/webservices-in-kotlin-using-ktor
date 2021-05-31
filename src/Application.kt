cee4bd36810d0b56850f61b3eb4f866e12c1dc84
SYSTEMS\muhammadarsalan.khan <muhammadarsalan.khan@systemsltd.com>
2021-05-31 14:56:20 +0500

    few changes

--- src/Application.kt
+++ src/Application.kt
@@ -9,7 +9,6 @@ import io.ktor.serialization.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
-@Suppress("unused") // Referenced in application.conf
 fun Application.module() {
     install(ContentNegotiation) {
         json()
